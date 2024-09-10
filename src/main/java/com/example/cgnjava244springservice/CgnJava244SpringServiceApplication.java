f9725fed9b772b30b7813487896af36a9d7932c2
mpagels <54703643+mpagels@users.noreply.github.com>
2024-09-10 14:19:08 +0200

    inital commit - setup project
    - spring initalizr
    - spring web
    - spring mongo data
    - lombok

---
+++ src/main/java/com/example/cgnjava244springservice/CgnJava244SpringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cgnjava244springservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgnJava244SpringServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgnJava244SpringServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cgnjava244springservice/CgnJava244SpringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cgnjava244springservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgnJava244SpringServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
